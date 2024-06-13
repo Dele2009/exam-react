6306e3acd82a3c3c5c434894b12f743e78ade064
Bamidele Aminu <obedaminu303@gmail.com>
2024-06-13 13:19:31 +0100

    ui text update

--- src/components/navigation/nav.jsx
+++ src/components/navigation/nav.jsx
@@ -10,7 +10,7 @@ const Navbar = ({ children }) => {
 
 
 
-            <header className="text-slate-700 absolute top-0 w-full z-50 mx-auto flex flex-col overflow-hidden px-4 py-5 lg:flex-row lg:items-center">
+            <header className="backdrop-blur-lg md:backdrop-blur-none text-slate-700 absolute top-0 w-full z-50 mx-auto flex flex-col overflow-hidden px-0  md:px-4 py-5 lg:flex-row lg:items-center">
                 <Link to="#" className="flex items-center whitespace-nowrap text-4xl font-black">
                     <span className="mr-2 w-8">
                         <img src="/images/JOJj79gp_Djhwdp_ZOKLL.png" alt="" />
@@ -23,7 +23,7 @@ const Navbar = ({ children }) => {
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M4 6h16M4 12h16M4 18h16"></path>
                     </svg>
                 </label>
-                <nav aria-label="Header Navigation" className="peer-checked:pt-8 peer-checked:max-h-60 flex max-h-0 w-full flex-col lg:ml-auto items-center overflow-hidden transition-all lg:ml-24 lg:max-h-full lg:flex-row">
+                <nav aria-label="Header Navigation" className="backdrop-blur-lg md:backdrop-blur-none peer-checked:pt-8 peer-checked:max-h-60 flex max-h-0 w-full flex-col lg:ml-auto items-center overflow-hidden transition-all lg:ml-24 lg:max-h-full lg:flex-row">
                     <ul  className="flex w-full flex-col items-center space-y-2 lg:flex-row lg:justify-center lg:space-y-0">
                         <li style={fontVarient} className="lg:mr-12"><Link className="rounded ca  text-slate-100 transition focus:outline-none focus:ring-1 focus:ring-blue-700 focus:ring-offset-2" to="#">Components</Link></li>
                         <li style={fontVarient} className="lg:mr-12"><Link className="rounded ca text-slate-100 transition focus:outline-none focus:ring-1 focus:ring-blue-700 focus:ring-offset-2" to="#">Pricing</Link></li>
