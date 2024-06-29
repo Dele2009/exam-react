926b79114519c6bbf963615ce9680243869b05f2
Bamidele Aminu <obedaminu303@gmail.com>
2024-06-29 21:03:39 +0100

    axios base url update

--- src/utilities/axios.jsx
+++ src/utilities/axios.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
-
+const url = 'https://schoolsmartsapi.vercel.app'
+// const url = 'http://localhost:5000'
 const instance = axios.create({
-  baseURL: 'http://localhost:5000', // Adjust the base URL as needed
+  baseURL: url, // Adjust the base URL as needed
 });
 
 export default instance;
