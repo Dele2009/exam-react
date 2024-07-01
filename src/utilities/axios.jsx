b105363aa418ae60ecc036b6f91545b1293fd47a
Bamidele Aminu <obedaminu303@gmail.com>
2024-07-01 17:59:55 +0100

    takeExam update

--- src/utilities/axios.jsx
+++ src/utilities/axios.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
-// const url = 'https://schoolsmartsapi.vercel.app'
-const url = 'http://localhost:5000'
+const url = 'https://schoolsmartsapi.vercel.app'
+// const url = 'http://localhost:5000'
 const instance = axios.create({
   baseURL: url, // Adjust the base URL as needed
 });
